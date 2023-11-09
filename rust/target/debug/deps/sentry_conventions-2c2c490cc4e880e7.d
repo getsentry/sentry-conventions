8bb86e676a64df4e7a461794a8b89ae1b048ad1f
Abhijeet Prasad <aprasad@sentry.io>
2023-11-09 11:30:20 -0500

    ignore rust target

--- rust/target/debug/deps/sentry_conventions-2c2c490cc4e880e7.d
+++
@@ -1,6 +0,0 @@
-/Users/abhijeetprasad/workspace/sentry-conventions/rust/target/debug/deps/sentry_conventions-2c2c490cc4e880e7.rmeta: src/lib.rs src/op.rs
-
-/Users/abhijeetprasad/workspace/sentry-conventions/rust/target/debug/deps/sentry_conventions-2c2c490cc4e880e7.d: src/lib.rs src/op.rs
-
-src/lib.rs:
-src/op.rs:
