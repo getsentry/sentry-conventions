c04c75106a9c79f48466f7bce49f7007a949adb5
Abhijeet Prasad <aprasad@sentry.io>
2024-11-29 11:55:39 -0500

    fix: adjust examples and add github workflow

--- test/validate-attribute.test.ts
+++ test/validate-attribute.test.ts
@@ -8,16 +8,61 @@ import attributeSchema from '../schemas/attribute.schema.json';
 
 const traceFolders = path.resolve(__dirname, '../model/trace');
 
-describe('attribute json follows json schema', async () => {
-  const files = await fs.promises.glob(`${traceFolders}/**/*.json`);
-  for await (const file of files) {
-    const filename = path.basename(file);
+describe('attribute json', async () => {
+  const filesIterator = await fs.promises.glob(`${traceFolders}/**/*.json`);
+  const files = await Array.fromAsync(filesIterator);
 
-    it(`${filename}`, async () => {
-      const ajv = new Ajv();
+  for (const file of files) {
+    const name = path.basename(file);
+    describe(name, async () => {
       const content = JSON.parse(await fs.promises.readFile(file, 'utf-8'));
-      ajv.validate(attributeSchema, content);
-      expect(ajv.errors).toBe(null);
+
+      it('should follow the attribute json schema', () => {
+        const ajv = new Ajv();
+        ajv.validate(attributeSchema, content);
+        expect(ajv.errors).toBe(null);
+      });
+
+      it('should set pii to false if type is not string or string[]', () => {
+        if (content.type !== 'string' && content.type !== 'string[]') {
+          expect(content.pii.key).toBe('false');
+        }
+      });
+
+      it('should have an example that follows the type set unless has_dynamic_suffix is true', () => {
+        if (content.has_dynamic_suffix) {
+          expect(typeof content.example).toBe('string');
+          return;
+        }
+
+        switch (content.type) {
+          case 'integer':
+          case 'number':
+            expect(typeof content.example).toBe('number');
+            break;
+          case 'integer[]':
+          case 'number[]':
+            expect(Array.isArray(content.example)).toBe(true);
+            expect(content.example.every((e: number) => typeof e === 'number')).toBe(true);
+            break;
+          case 'string':
+            expect(typeof content.example).toBe('string');
+            break;
+          case 'string[]':
+            expect(Array.isArray(content.example)).toBe(true);
+            expect(content.example.every((e: string) => typeof e === 'string')).toBe(true);
+            break;
+          case 'boolean':
+            expect(typeof content.example).toBe('boolean');
+            break;
+          case 'boolean[]':
+            expect(Array.isArray(content.example)).toBe(true);
+            expect(content.example.every((e: boolean) => typeof e === 'boolean')).toBe(true);
+            break;
+          default:
+            throw new Error('Invalid type');
+        }
+      });
     });
   }
 });
