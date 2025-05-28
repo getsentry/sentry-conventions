f92acaccfe62bfa8513a896869d51fdccd497745
Shruthi <s3jaganathan@gmail.com>
2025-05-28 09:17:15 -0400

    feat: Add a script to generate deprecated attributes (#61)
    Adds a script to generate a JSON of deprecated fields
    so the ingest pipeline can use sentry-conventions
    as the source of truth.
    
    Also adds a _status field to the attributes schema.
    We can use this internally do decide which stage of the
    deprecation an is in. Deprecated fields need a 90 day
    backfill (ie relay double-writes, EAP coalesces etc) after
    which they can just be normalized to the new field.

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,7 +1,9 @@
 import { generateAttributeDocs } from './generate_attribute_docs';
 import { generateAttributes } from './generate_attributes';
+import { generateDeprecatedAttributesJSON } from './generate_deprecated_attributes_json';
 import { generateOps } from './generate_op';
 
 generateOps();
 generateAttributes();
 generateAttributeDocs();
+generateDeprecatedAttributesJSON();
