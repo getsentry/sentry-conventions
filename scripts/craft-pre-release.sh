6d6da468a4dad5287639128c964ee6f15b0cea02
Lorenzo Cian <lorenzo.cian@sentry.io>
2025-09-12 10:54:21 +0200

    meta(release): Fix release workflow and script (#144)

--- scripts/craft-pre-release.sh
+++ scripts/craft-pre-release.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 set -eux
+
+ROOT_DIR="$(realpath $(dirname "$0")/..)"
+
 OLD_VERSION="${1}"
 NEW_VERSION="${2}"
 
+echo "Current version: $OLD_VERSION"
+echo "Bumping to version: $NEW_VERSION"
+
+# ==================== JS ====================
+
+cd $ROOT_DIR
+cd javascript/sentry-conventions
+
 # Do not tag and commit changes made by "npm version"
 export npm_config_git_tag_version=false
-(cd javascript/sentry-conventions && yarn version "${NEW_VERSION}")
+
+npm version "${NEW_VERSION}"
+
+# ==================== PY ====================
+
+cd $ROOT_DIR
+cd python
+
+uv version "$NEW_VERSION"
